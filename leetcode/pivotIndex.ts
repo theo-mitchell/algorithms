c0eb7c5e66442bc0825f9eb69097af960a729a93
theom95 <theodore.mitchell95@gmail.com>
2022-10-23 15:24:48 -0500

    added better solution for pivotIndex

--- leetcode/pivotIndex.ts
+++ leetcode/pivotIndex.ts
@@ -9,7 +9,7 @@ Return the leftmost pivot index. If no such index exists, return -1.
 
 // Very janky initial solution.
 
-function pivotIndex(nums: number[]): number {
+function pivotIndexBad(nums: number[]): number {
     let pI = -1
     for (var i = 0; i < nums.length; i++) {
         let left = nums.slice(0, i);
@@ -31,4 +31,24 @@ function pivotIndex(nums: number[]): number {
     }
 
     return pI;
+};
+
+// Better way
+function pivotIndex(nums: number[]): number {
+    let pivotIndex = -1;
+    const sum: number = nums.reduce((partialSum, a) => partialSum + a, 0);
+    let leftSum: number = 0;
+
+    for (var i = 0; i < nums.length; i++) {
+
+        if (leftSum === sum - nums[i] - leftSum) {
+            pivotIndex = i;
+            console.log('assigned pi')
+            break;
+        }
+
+        leftSum += nums[i];
+    }
+
+    return pivotIndex;
 };
